fa16a85b399a8a08bf6fef2c9afc8abba7365ad3
BlondeNamazu <namazutitech15@gmail.com>
2022-02-07 23:20:58 +0900

    modify DetailView

--- app/src/main/java/com/example/pokeapp/detail/DetailActivity.kt
+++ app/src/main/java/com/example/pokeapp/detail/DetailActivity.kt
@@ -8,6 +8,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -18,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.navArgs
 import coil.compose.rememberImagePainter
 import com.example.pokeapp.ui.theme.PokeAppTheme
@@ -57,16 +60,53 @@ fun PokemonDetailItem(viewModel: DetailViewModel) {
     when (val state = viewModel.state.observeAsState().value) {
         is DetailViewModel.State.Initialized.Ideal -> {
             val pokemonInfo = state.pokemonInfo
-            Row {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(
+                    text = "Pokedex Number: ${pokemonInfo.id}",
+                    fontSize = 16.sp
+                )
                 Image(
                     painter = rememberImagePainter(pokemonInfo.imageUrl),
                     contentScale = ContentScale.Crop,
                     contentDescription = null,
-                    modifier = Modifier.size(64.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp)
                 )
-                Column {
-                    Text(text = pokemonInfo.name)
-                    Text(text = pokemonInfo.types.toString())
+                Text(
+                    text = pokemonInfo.name,
+                    fontSize = 64.sp
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    LazyColumn(
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        items(pokemonInfo.types) { type ->
+                            Text(text = type)
+                        }
+                    }
+                    Column(
+                        verticalArrangement = Arrangement.SpaceAround,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text("HP: ${pokemonInfo.statInfo.hp}")
+                        Text("Attack: ${pokemonInfo.statInfo.attack}")
+                        Text("Defense: ${pokemonInfo.statInfo.defense}")
+                        Text("SpecialAttack: ${pokemonInfo.statInfo.specialAttack}")
+                        Text("SpecialDefense: ${pokemonInfo.statInfo.specialDefense}")
+                        Text("Speed: ${pokemonInfo.statInfo.speed}")
+                    }
                 }
             }
         }
