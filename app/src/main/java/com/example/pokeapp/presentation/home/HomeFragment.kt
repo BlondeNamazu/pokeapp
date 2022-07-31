c57da859c0a1a3fd1743431310c4134a19ebfdd5
BlondeNamazu <namazutitech15@gmail.com>
2022-07-31 17:35:47 +0900

    setup bottom navigation bar

--- app/src/main/java/com/example/pokeapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokeapp/presentation/home/HomeFragment.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.findNavController
+import com.example.pokeapp.presentation.favorite.FavoriteScreen
 import com.example.pokeapp.presentation.search.SearchScreen
 import com.example.pokeapp.presentation.search.SearchViewModel
 import com.example.pokeapp.ui.theme.PokeAppTheme
@@ -24,16 +35,60 @@ class HomeFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 PokeAppTheme {
-                    SearchScreen(
-                        viewModel = searchViewModel,
-                        onClickItem = { id ->
-                            findNavController().navigate(
-                                HomeFragmentDirections.actionHomeToDetail(id)
-                            )
+                    val parentNavController = findNavController()
+                    val homeNavController = rememberNavController()
+                    Scaffold(
+                        bottomBar = { BottomNavigationBar(homeNavController) }
+                    ) {
+                        NavHost(
+                            navController = homeNavController,
+                            startDestination = BottomNavigationDestination.Search.label
+                        ) {
+                            composable(BottomNavigationDestination.Search.label) {
+                                SearchScreen(
+                                    viewModel = searchViewModel,
+                                    onClickItem = { id ->
+                                        parentNavController.navigate(
+                                            HomeFragmentDirections.actionHomeToDetail(id)
+                                        )
+                                    }
+                                )
+                            }
+                            composable(BottomNavigationDestination.Favorite.label) {
+                                FavoriteScreen()
+                            }
                         }
-                    )
+                    }
                 }
             }
+        }
+    }
+}
+
+enum class BottomNavigationDestination(
+    val icon: ImageVector,
+    val label: String
+) {
+    Search(Icons.Filled.Home, "Home"),
+    Favorite(Icons.Filled.Favorite, "Favorite");
+}
+
+@Composable
+fun BottomNavigationBar(
+    navController: NavController
+) {
+    var selectedItem by remember { mutableStateOf(0) }
+    BottomNavigation {
+        BottomNavigationDestination.values().forEachIndexed { index, destination ->
+            BottomNavigationItem(
+                icon = { Icon(destination.icon, destination.label) },
+                label = { Text(destination.label) },
+                selected = index == selectedItem,
+                onClick = {
+                    selectedItem = index
+                    navController.navigate(destination.label)
+                }
+            )
         }
     }
 }
